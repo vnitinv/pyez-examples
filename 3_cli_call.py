c4ca8c7440398495b5917548443cb754bdc10109
nitinkr <nitinkr@juniper.net>
2019-03-04 11:48:33 +0530

    using context manager

--- 3_cli_call.py
+++ 3_cli_call.py
@@ -1,8 +1,4 @@
 from jnpr.junos import Device
 
-dev = Device(host='xxxx', user='demo', password='demo123', gather_facts=False)
-dev.open()
-
-print dev.cli("show version", warning=False)
-
-dev.close()
+with Device(host='xxxx', user='demo', password='demo123') as dev:
+    print (dev.cli("show version", warning=False))
